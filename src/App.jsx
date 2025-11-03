56d9eb5bb0efac1328b5394b9430d7513db97f89
LautaroM00 <165226383+LautaroM00@users.noreply.github.com>
2025-11-03 17:29:40 -0300

    Removed test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './Screens/Home'
-import Test from './Components/Test'
 import Services from './Screens/Services'
 import BookNowFloat from './Components/BookNowFloat'
 import Booking from './Components/Booking'
@@ -20,7 +19,6 @@ function App() {
           <Route path='/booking' element={<Booking />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/services' element={<Services />} />
-          <Route path='/test' element={<Test />} />
           <Route path='/accommodations' element={<Accommodation />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
