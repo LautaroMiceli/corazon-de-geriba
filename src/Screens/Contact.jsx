42c00ac988118b74986fb551cd403b31e1a09f7a
LautaroM00 <165226383+LautaroM00@users.noreply.github.com>
2025-11-10 14:31:05 -0300

    add tiktok button.

--- src/Screens/Contact.jsx
+++ src/Screens/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaInstagram, FaWhatsapp, FaEnvelope, FaPhone } from 'react-icons/fa';
 import Map from '../Components/Map';
 import { useTranslation } from 'react-i18next';
+import { FaTiktok } from 'react-icons/fa6';
 
 export default function FooterContact() {
   const {t} = useTranslation()
@@ -64,6 +65,10 @@ const FollowUs = () => {
       href: "https://wa.me/5522999999999",
       component: <FaWhatsapp />
     },
+    {
+      href: "https://www.tiktok.com/@corao.de.gerib.gue",
+      component: <FaTiktok />
+    }
   ];
   return (
     <div className="flex w-full flex-col gap-2">
